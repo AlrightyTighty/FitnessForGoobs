62d624d76ec1accb91e5270d1a4d05cfa35b253e
alrightytighty <ileikpotatozzzz@gmail.com>
2025-03-21 20:06:03 -0500

    transfer everything

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import TopNav from "./Components/TopNav";
+
+function App() {
+  return (
+    <div>
+      <TopNav/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/TopNav.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom"
+import "./TopNav.css"
+
+const TopNav = () => {
+    return (
+    <div className="topnav">
+        <div className="left-content">
+            <span>
+                Goob Fitness
+            </span>
+        </div>
+        <div className="right-content">
+            <Link to="/">Account</Link>
+            <Link to="/">Community</Link>
+            <Link to="/">Exercises</Link>
+        </div>
+    </div>
+    );
+}
+
+export default TopNav;
